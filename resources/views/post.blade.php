89f9f00ef0c44534f9ac134b70a3ca04a9141644
Ibrahim Al-Ashqar <eng.ibrahim20191406@gmail.com>
2023-05-13 18:29:40 +0300

    Blade with a few tweaks

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,23 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Post</title>
-  <link rel="stylesheet" href="../css/style.css">
-</head>
-
-<body>
+<x-layout>
   <article>
-    <h1><?= $post->title ?></h1>
+    <h1>{{ $post->title }}</h1>
     <div>
-      <?= $post->body ?>
+      {!! $post->body !!}
     </div>
   </article>
 
   <a href="/">Go Back</a>
-</body>
-
-</html>
+</x-layout>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,28 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
+<x-layout>
+  @foreach ($posts as $post)
+    <article class="{{ $loop->even ? 'foobar' : '' }}">
+      <h1>
+        <a href="posts/{{ $post->slug }}">
+          {{ $post->title }}
+        </a>
+      </h1>
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Blog</title>
-  <link rel="stylesheet" href="css/style.css">
-</head>
-
-<body>
-  <?php foreach ($posts as $post) : ?>
-  <article>
-    <h1>
-      <a href="posts/<?= $post->slug ?>">
-        <?= $post->title ?>
-      </a>
-    </h1>
-
-    <div>
-      <?= $post->excerpt ?>
-    </div>
-  </article>
-  <?php endforeach; ?>
-</body>
-
-</html>
+      <div>
+        {{ $post->excerpt }}
+      </div>
+    </article>
+  @endforeach
+</x-layout>
