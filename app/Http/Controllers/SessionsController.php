970b25e5201364bb305ba51b2555972a1451e5ee
Ibrahim Al-Ashqar <eng.ibrahim20191406@gmail.com>
2023-06-15 05:52:26 +0300

    Forms and Authentication

---
+++ app/Http/Controllers/SessionsController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+
+class SessionsController extends Controller
+{
+    public function create()
+    {
+        return view('sessions.create');
+    }
+
+    public function store()
+    {
+        $attributes = request()->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required', 'min:7']
+        ]);
+
+        if (!auth()->attempt($attributes)) {
+            throw ValidationException::withMessages([
+                'email' => 'Your provided credentials could not be verfied.',
+            ]);
+        }
+        
+        session()->regenerate();
+
+        return redirect('/')->with('success', 'Welcome Back!');
+    }
+
+    public function destroy()
+    {
+        auth()->logout();
+
+        return redirect('/')->with('success', 'Goodbye!');
+    }
+}
